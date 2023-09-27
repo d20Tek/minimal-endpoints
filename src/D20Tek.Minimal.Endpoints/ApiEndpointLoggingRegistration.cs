c270151036e66e8b34d29afee86c42a2adf546bc
Pedro Silva <pedrosi@microsoft.com>
2023-09-26 17:20:37 -0700

    Added logging middleware and registration for ApiEndpoints to log all incoming requests and outgoing responses.

---
+++ src/D20Tek.Minimal.Endpoints/ApiEndpointLoggingRegistration.cs
@@ -0,0 +1,24 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace D20Tek.Minimal.Endpoints;
+
+public static class ApiEndpointLoggingRegistration
+{
+    private static ApiEndpointLoggingFilter _endpointLogger = new();
+
+    public static IApplicationBuilder UseApiEndpointLogging(this IApplicationBuilder app)
+    {
+        app.Use(InvokeHandler);
+
+        return app;
+    }
+
+    internal static async Task InvokeHandler(HttpContext context, Func<Task> next)
+    {
+        await _endpointLogger.InvokeAsync(context, next);
+    }
+}
